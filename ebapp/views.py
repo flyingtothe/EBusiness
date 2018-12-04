59d63f7dc1163d2e0c02969b1a7567f55b200277
= <=>
2018-12-04 17:19:17 +0800

    18/12/4 modify

--- ebapp/views.py
+++ ebapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-class Login():
-    pass
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from ebapp.models import *
+from ebapp.serializers import *
+
+class RegisterAPIView(ModelViewSet):
+    queryset = Users.objects.all()
+    serializer_class = UsersSer
