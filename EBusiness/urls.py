6abb758c8886ff7bb3c6b3a9b9b7ec94dad957f5
= <=>
2018-12-05 17:29:26 +0800

    18/12/5 modify

--- EBusiness/urls.py
+++ EBusiness/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from ebapp import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login),
-]
+] + static(settings.STATIC_URL)
+
